b95debc00857a1731235191853f15d082df22072
SeddikKadi <seddik.kadi@outlook.fr>
2021-02-21 10:21:49 +0100

    product component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-
-
-function App() {
-
+import data from "./data.json"
+import react from "react"
+import Products from "./components/products"
+class App extends react.Component {
+  constructor(){
+    super();
+    this.state={
+      prosucts:data.products,
+      size:"",
+      sort:""
+    }
+  }
   //feature -1 test branche commit
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">Sopping Cart</a>
-      </header>
-      <main>
-        product List 
-      </main>
-      <footer>
-        All rights reserved.
-      </footer>
+  render(){
+  
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">Sopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
 
-    </div>
-  );
+          <div className="main">
+            <Products products={this.state.prosucts}/>
+          </div>
+          <div className="sidebar">
+            cart Items
+          </div>
+
+          </div>
+        </main>
+        <footer>
+          All rights reserved.
+        </footer>
+  
+      </div>
+    );
+  }
+
 }
 
 export default App;
